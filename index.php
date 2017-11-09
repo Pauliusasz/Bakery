8a67345f5894ffc6549fae6cc72311ccebbe5cb0
Paulius <pauliusasz@gmail.com>
2017-11-09 18:54:50 +0200

    Added table

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bakery</title>
+</head>
+<body>
+<table>
+	
+	<thead>
+		
+		<td>Pavadinimas</td>
+
+	</thead>
+
+	<tbody>
+		<tr>
+			
+			<td>Aguoninė</td>
+
+		</tr>
+		<tr>
+			
+			<td>Varškės</td>
+
+		</tr>
+		<tr>
+			
+			<td>Duona balta</td>
+
+		</tr>
+		<tr>
+			
+			<td>Duona juoda</td>
+
+		</tr>
+		<tr>
+			
+			<td>Duona ruginė</td>
+		</tr>
+	</tbody>
+
+</table>
+
+
+</body>
+</html>
