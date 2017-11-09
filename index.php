e46ff60d747589ebec6986bf91266943632b1c3b
Paulius <pauliusasz@gmail.com>
2017-11-09 19:33:17 +0200

    Added final balance

--- index.php
+++ index.php
@@ -10,35 +10,67 @@
 <table>
 	
 	<thead .thead>
+		<tr>
 		
-		<td>Pavadinimas</td>
+			<th rowspan="2">Pavadinimas</th>
+			<th colspan="5">2017-11-09</th>
+
+		</tr>
+		<tr>
+			<th>VL</th> <!-- Vakarykštis likutis -->
+			<th>PG</th> <!--Pagaminta-->
+			<th>PR</th> <!--Parduota-->
+			<th>SG</th> <!--Sugadinta-->
+			<th>GL</th> <!--Galutinis likutis-->
+
+		</tr>
 
 	</thead>
 
 	<tbody>
-		<tr>
-			
+		<tr>			
 			<td>Aguoninė</td>
-
+			<td>5</td>
+			<td>20</td>
+			<td>19</td>
+			<td>4</td>
+			<td>2</td>
 		</tr>
 		<tr>
 			
 			<td>Varškės</td>
-
+			<td>1</td>
+			<td>25</td>
+			<td>17</td>
+			<td>0</td>
+			<td>7</td>
 		</tr>
 		<tr>
 			
 			<td>Duona balta</td>
-
+			<td>9</td>
+			<td>16</td>
+			<td>10</td>
+			<td>3</td>
+			<td>12</td>
 		</tr>
 		<tr>
 			
 			<td>Duona juoda</td>
-
+			<td>0</td>
+			<td>25</td>
+			<td>19</td>
+			<td>1</td>
+			<td>6</td>
 		</tr>
 		<tr>
 			
 			<td>Duona ruginė</td>
+			<td>3</td>
+			<td>15</td>
+			<td>17</td>
+			<td>1</td>
+			<td>0</td>
 		</tr>
 	</tbody>
 
