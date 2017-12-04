380908dd35d7984425041913c5e6afe94097aa08
Paulius <pauliusasz@gmail.com>
2017-12-04 20:06:48 +0200

    Added construct function

--- app/dto/Bun.php
+++ app/dto/Bun.php
@@ -3,18 +3,11 @@
 class Bun
 {
 
-	const OWNER = 'Bakery Inc.';
+	public function __construct ()
+	{
+		echo 'hello' . "\r\n";
+	}
 
-	public static $OWNER_TWO = 'UAB Kepykla';
-
-	private $a = 'A';
-
-	protected $b = 'B';
-
-	public $c = 'C';
 }
 
-$bun = new Bun();
-// print_r($bun->c);
-print_r(Bun::OWNER);
-print_r(Bun::$OWNER_TWO);
+$bun= new Bun();
